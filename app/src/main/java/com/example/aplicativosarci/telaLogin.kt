d18869ee7d2a6836c97df5dda5397a7017107382
kaiovictor <kaio.victor@academico.ifpb.edu.br>
2025-11-07 02:56:39 -0300

    Modificando os arquivos

--- app/src/main/java/com/example/aplicativosarci/telaLogin.kt
+++ app/src/main/java/com/example/aplicativosarci/telaLogin.kt
@@ -9,12 +9,6 @@ import androidx.core.view.WindowInsetsCompat
 class telaLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_tela_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
